9893569a9d5212d3c9c654fa4e09a78617d421a7
saurav1207 <sauravkumar843114@gmail.com>
2023-05-05 22:56:15 +0530

    worked on navbar and added login/signup pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Contact from "./Components/Contact";
 import About from "./Components/About";
 import Services from "./Components/Services";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
     <Route path="about" element={ <About/> } />
     <Route path="contact" element={ <Contact/> } />
     <Route path="services" element={ <Services/> } />
+    <Route path="login" element={<Login/>}/>
+    <Route path="signup" element={<Signup/>}/>
     </Routes>
     </div>
   );
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,24 +1,21 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-
-import './Home.css';
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
 
 function Home() {
   return (
     <div>
       <nav>
-  <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/about">About Us</a></li>
-    <li><a href="/services">Services</a></li>
-    <li><a href="contact">Contact Us</a></li>
-  </ul>
-</nav>
-
-
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="contact">Contact Us</a></li>
+          <li className="right"><Link to="/login">Login / Sign Up</Link></li> {/* Added the right class to the Login/Sign Up button */}
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
