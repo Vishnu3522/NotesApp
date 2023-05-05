f4aaf01a0d642c5793413db2c874850a2a04f91b
vishnu <vishnukumar.it24@jecrc.ac.in>
2023-05-05 22:08:44 +0530

    banner applied on home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 import './Home.css';
 
@@ -15,9 +15,40 @@ function Home() {
     <li><a href="contact">Contact Us</a></li>
   </ul>
 </nav>
-
-
+<div className='banner'>
+  <div
+    style={{
+      position: "relative",
+      width: "100%",
+      height: 0,
+      paddingTop: "75.0000%",
+      paddingBottom: 0,
+      boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
+      marginTop: "1.6em",
+      marginBottom: "0.9em",
+      overflow: "hidden",
+      borderRadius: 8,
+      willChange: "transform"
+    }}
+  >
+    <iframe
+      loading="lazy"
+      style={{
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        top: 0,
+        left: 0,
+        border: "none",
+        padding: 0,
+        margin: 0
+      }}
+      src="https://www.canva.com/design/DAFiD-Q86LE/view?embed"
+    ></iframe>
     </div>
+    </div>
+    </div>
+
   )
 }
 
